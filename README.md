a16485967784dc3a3c726034263e8bd2b815c001
Viljami Kuosmanen <viljami@viljami.io>
2026-02-17 08:50:58 +0200

    🌅

---
+++ README.md
@@ -0,0 +1,5 @@
+# agenthole
+
+```
+npx agenthole
+```
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+import packageJson from "../package.json" with { type: "json" };
+
+const main = () => {
+  console.log(`agenthole v${packageJson.version}`);
+  process.exit(0);
+};
+
+main();
